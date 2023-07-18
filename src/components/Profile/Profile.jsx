dbf6ff2f8af31c7ebc60514bf43922492740a3c9
KostiZin <pancostian@gmail.com>
2023-07-18 19:23:52 -0400

    update

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,15 +1,5 @@
 import user from './user.json';
 
-{
-  /* <Profile
-  username={user.username}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-  stats={user.stats}
-/>; */
-}
-
 export const Profile = () => {
   const {
     username,
