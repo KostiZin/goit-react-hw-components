1edfc08a1552d288419ecf54c3912b63b0b775b2
KostiZin <pancostian@gmail.com>
2023-07-18 19:30:10 -0400

    App hide

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App/App';
+// import { App } from 'components/App/App';
 import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
     <Profile />
     <Statistics />
   </React.StrictMode>
